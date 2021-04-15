cb6a0661b9f76f23c96616e70cd9039e1affffd4
Seraph <82589602+Seraph-77@users.noreply.github.com>
2021-04-15 08:40:20 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quizzapptesting
+
+i own nothing here. i followed a tutorial and 
+used a little bit of old code from when i tried to make a website last year
+
+i own nothing in here, except the image for "Makoot"
+
+-@~Kowareta~#7959
---
+++ script.js
@@ -0,0 +1,124 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  },
+  
+  {
+    question: 'whats 4x5?',
+    answers: [
+      { text: '25', correct: false },
+      { text: '20', correct: true },
+      { text: '30', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  },
+  {
+    question: 'who was the first president of the USA?',
+    answers: [
+      {text: 'George Washington', correct: true},
+      {text: 'Barack Obama', correct: false},
+      {text: 'mohammad ali', correct: false},
+      {text: 'Andrew Jackson', correct: false}
+
+    ]
+  }, 
+  {
+    question: 'johnny has 34 apples, and he shares 10. how many apples does he have left',
+    answers: [
+      {text: 'Johnny has 24 apples', correct: true},
+      {text: 'Johnny has 15 apples', correct: false},
+      {text: 'Johnny has 237 apples', correct: false},
+      {text: 'Johnny has 103 apples', correct: false}
+    ]
+
+  } 
+]
